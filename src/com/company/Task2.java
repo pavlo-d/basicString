7aa8725ce3c6ce73b70a20790350d901ea641ebf
Pavlo_Denysyuk <Pavlogh#1>
2020-07-21 14:13:02 +0300

    add description in tasks

---
+++ src/com/company/Task2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+	// Вивести на екран символи-дублікати та вказати їх кількість (наприклад в "Java" є 2 дублікати - "а")
+
+    }
+}
